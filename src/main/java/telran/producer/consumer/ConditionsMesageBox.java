0281573dc4748064b5641731a5f8dd8ed34d610d
Yuragranovsky <yuragranovsky@gmail.com>
2024-12-17 16:35:49 +0200

    introducing ConditionsMessageBox based on Lock

---
+++ src/main/java/telran/producer/consumer/ConditionsMesageBox.java
@@ -0,0 +1,53 @@
+package telran.producer.consumer;
+
+import java.util.concurrent.locks.*;
+
+public class ConditionsMesageBox implements MessageBox{
+    private Lock lock = new ReentrantLock();
+    private Condition waitingForConsumer = lock.newCondition();
+    private Condition waitingForProducer = lock.newCondition();
+    private String message;
+    @Override
+    public void put(String message) throws InterruptedException {
+       try {
+        lock.lock();
+        while(this.message != null) {
+            waitingForConsumer.await();
+        }
+        this.message = message;
+        waitingForProducer.signal();
+       }finally {
+            lock.unlock();
+       }
+    }
+
+    @Override
+    public String take() throws InterruptedException {
+        try {
+            lock.lock();
+            while(message == null) {
+                waitingForProducer.await();
+            }
+            String msg = message;
+            message = null;
+            waitingForConsumer.signal();
+            return msg;
+           }finally {
+                lock.unlock();
+           }
+    }
+
+    @Override
+    public String poll() {
+        try {
+            lock.lock();
+            String msg = message;
+            message = null;
+            waitingForConsumer.signal();
+            return msg;
+           }finally {
+                lock.unlock();
+           }
+    }
+
+}
